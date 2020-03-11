c42d428d211da6d51fb2ed3256842408f66cb338
Alex Kellner <alexander.kellner@in2code.de>
2020-03-11 12:50:39 +0100

    [TASK] Update for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,20 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'OpenStreetMap for tt_address',
 	'description' => 'This extends ods_osm to use tt_address records.',
 	'author' => 'Robert Heel',
 	'author_email' => 'typo3@bobosch.de',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'ods_osm' => '2.0.0-',
 			'tt_address' => '3.1.0-',
-			'typo3' => '6.2.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+			'typo3' => '6.2.0-9.5.99',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
 	'state' => 'stable',
-	'version' => '3.0.3',
-);
-?>
+	'version' => '4.0.0',
+];
